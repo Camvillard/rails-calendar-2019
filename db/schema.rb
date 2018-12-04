0c887e2ccc58bfa7a623af2df4584e3d46a9f860
camille villard <hello@camillevillard.com>
2018-12-04 18:00:29 -0500

    added price to products with moetize

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_225541) do
+ActiveRecord::Schema.define(version: 2018_12_04_225817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_225541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cover_image", default: "", null: false
+    t.integer "price_cents", default: 0, null: false
   end
 
   create_table "shippings", force: :cascade do |t|
