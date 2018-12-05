a68d7a23d35803c166e4aa05ae92e4a5681fe4b8
camille villard <hello@camillevillard.com>
2018-12-05 11:13:22 -0500

    implementend errors messages & email validation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,25 @@ class OrdersController < ApplicationController
 
   def create
   calendar = Product.find(params[:calendar_id])
-  order  = Order.create!(
+  order  = Order.new(
     product_name: calendar.name,
     amount: calendar.price,
     state: "pending",
     user_email: params[:user_email]
     )
 
-  redirect_to new_order_payment_path(order)
-end
+  if order.save
+    redirect_to new_order_payment_path(order)
+  else
+    # raise
+    if order.errors.messages[:user_email].first == "can't be blank"
+      flash[:alert] = "email #{order.errors.messages[:user_email].first}"
+      redirect_to root_path
+    else
+      flash[:alert] = "something went wrong. please try again or send me an email!"
+      redirect_to root_path
+    end
+  end
+
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   monetize :amount_cents
+
+  validates :user_email, presence: true
 end
