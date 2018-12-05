0d0cba9377ce6a8bace29f30af3aebf9b60e66f8
camille villard <hello@camillevillard.com>
2018-12-04 19:18:50 -0500

    implemented user email for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
   def create
   calendar = Product.find(params[:calendar_id])
-  order  = Order.create!(product_name: calendar.name, amount: calendar.price, state: "pending")
+  order  = Order.create!(
+    product_name: calendar.name,
+    amount: calendar.price,
+    state: "pending",
+    user_email: params[:user_email]
+    )
 
   redirect_to new_order_payment_path(order)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_235107) do
+ActiveRecord::Schema.define(version: 2018_12_05_000808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_235107) do
     t.jsonb "payment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_email"
   end
 
   create_table "product_images", force: :cascade do |t|
