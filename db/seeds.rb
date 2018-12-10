43333e293967542b7699b6770d3bff9dbc4f40eb
camille villard <hello@camillevillard.com>
2018-12-10 11:53:46 -0500

    updated calendar price in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ janvier à décembre, avec un petit coin pour glisser toutes les notes du mois +
 24’ x 32’ (environ 60 x 80 cm)
 imprimé sur un papier blanc lisse
 douze feuilles reliées.",
-price_cents: 5000,
+price_cents: 5200,
 )
 
 standard = Shipping.create(
